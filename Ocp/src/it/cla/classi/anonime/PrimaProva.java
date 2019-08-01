18a590577747bc246f46eda593b139968f0a0a3c
cleud19 <iancuclaudio19@gmail.com>
2019-08-01 22:54:52 +0200

    Prima Classe aggiunta per classi anonime

--- Ocp/src/it/cla/classi/anonime/PrimaProva.java
+++
@@ -1,5 +0,0 @@
-package it.cla.classi.anonime;
-
-public class PrimaProva {
-
-}
---
+++ Ocp/src/it/cla/classi/anonime/SuperClasse.java
@@ -0,0 +1,7 @@
+package it.cla.classi.anonime;
+
+public class SuperClasse {
+	public void metodo() {
+		System.out.println("Nella classe esistente!");
+	}
+}
