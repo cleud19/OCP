8fec664e588fca8283f08429ea1a7c68b686b254
cleud19 <iancuclaudio19@gmail.com>
2020-01-28 13:31:26 +0100

    test iniziale secondo tentativo

--- Ocp/src/main/java/it/cla/test/alvolo/Main.java
+++ Ocp/src/main/java/it/cla/test/alvolo/Main.java
@@ -1,11 +1,24 @@
 package it.cla.test.alvolo;
 
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.Period;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
 
 public class Main {
 
 	public static void main(String[] args) {
+		peekTest();
+	}
+
+	static public void peekTest() {
+		Stream<LocalDate> s = Stream.of(LocalDate.now());
+		UnaryOperator<LocalDate> u = l -> l;
+		s.filter(l -> l != null).map(u).peek(System.out::println);
+	}
+
+	static public void durationnPeriod() {
 		String d = Duration.ofDays(1).toString();
 		String p = Period.ofDays(1).toString();
 		System.out.println(d + " " + p);
