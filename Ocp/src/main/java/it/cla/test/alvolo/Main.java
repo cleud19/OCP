ab9dfb689e35c6a6ee0345013aa6df91f92ba779
cleud19 <iancuclaudio19@gmail.com>
2020-02-20 16:38:09 +0100

    risposte lavoro

--- Ocp/src/main/java/it/cla/test/alvolo/Main.java
+++ Ocp/src/main/java/it/cla/test/alvolo/Main.java
@@ -4,7 +4,9 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
@@ -12,7 +14,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// peekTest();
-		testCollection();
+		//testCollection();
+		robo();
+	}
+
+	private static void robo() {
+		Map<Integer, Integer> map = new HashMap<>();
+		map.put(1, 10);
+		map.put(2, 20);
+		map.put(3, null);
+		map.merge(1, 3, (a, b) -> a + b);
+		map.merge(3, 3, (a, b) -> a + b);
+		System.out.println(map);
 	}
 
 	static public void peekTest() {
